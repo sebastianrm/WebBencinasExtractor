8a8d8539650d0de086cba6774a1c9ecdada2fbb5
sebastianrm <retamal.sebastian@gmail.com>
2012-08-06 16:50:12 -0400

    sobreescrive metodo close

--- WebExtractor/src/cl/mobilLoyalti/webExtractor/db/ConnectionDB.java
+++ WebExtractor/src/cl/mobilLoyalti/webExtractor/db/ConnectionDB.java
@@ -12,145 +12,121 @@ import org.apache.log4j.Logger;
 
 public class ConnectionDB {
 	Logger logger = Logger.getLogger(ConnectionDB.class);
-	
-	public ConnectionDB(){
-		
+
+	public ConnectionDB() {
+
 	}
-	
-	/*
-	 * Cierra el objeto Statement
-	 * 
-	 * @param ps Objeto Statement.
-	 */	
-	private void closeSt(Statement st){
-		if(st != null){
-			try {
-				st.close();
-			} catch (SQLException e) {
-				logger.warn(e.getMessage(), e);
-			}finally {
-				st = null;
-			}
-		}
-	}
-	
-	/*
-	 * Cierra el objeto PreparedStatement
-	 * 
-	 * @param ps Objeto PreparedStatement.
-	 */	
-	private void closePs(PreparedStatement ps){
-		if(ps != null){
-			try {
-				ps.close();
-			} catch (SQLException e) {
-				logger.warn(e.getMessage(), e);
-			}finally {
-				ps = null;
-			}
-		}
-	}
-	
+
 	/*
 	 * Cierra el objeto ResultSet
 	 * 
 	 * @param rs Objeto ResultSet.
-	 */		
-	private void closeRs(ResultSet rs){
-		if(rs != null){
+	 */
+	private void close(ResultSet rs) {
+		if (rs != null) {
 			try {
 				rs.close();
 			} catch (SQLException e) {
 				logger.warn(e.getMessage(), e);
-			}finally {
+			} finally {
 				rs = null;
 			}
-		}		
+		}
 	}
 
 	/*
 	 * Cierra el objeto Connection
 	 * 
 	 * @param conn Objeto Connection.
-	 */			
-	private void closeConn(Connection conn){
-		if(conn != null){
+	 */
+	private void close(Connection conn) {
+		if (conn != null) {
 			try {
 				conn.close();
 			} catch (SQLException e) {
 				logger.warn(e.getMessage(), e);
-			}finally {
+			} finally {
 				conn = null;
 			}
-		}	
+		}
 	}
-	
+
 	/*
 	 * Cierra los objetos PreparedStatement y Connection simultaneamente.
 	 * 
-	 * @param ps   Objeto PreparedStatement. 
+	 * @param ps Objeto PreparedStatement.
+	 * 
 	 * @param conn Objeto Connection.
-	 */			
-	public void closeAll(PreparedStatement ps, Connection conn){
-		closePs(ps);
-		closeConn(conn);
+	 */
+	public void close(PreparedStatement ps, Connection conn) {
+		close(ps);
+		close(conn);
 	}
-	
+
 	/*
 	 * Cierra los objetos PreparedStatement.
 	 * 
-	 * @param ps Objeto PreparedStatement. 
-	 */			
-	public void closeAll(PreparedStatement ps){
-		closePs(ps);
+	 * @param ps Objeto PreparedStatement.
+	 */
+	public void close(PreparedStatement ps) {
+		try {
+			ps.close();
+		} catch (SQLException e) {
+			logger.error(e);
+		}
 	}
-	
+
 	/*
 	 * Cierra los objetos Statement.
 	 * 
-	 * @param ps Objeto Statement. 
-	 */			
-	public void closeAll(Statement st){
-		closeSt(st);
+	 * @param ps Objeto Statement.
+	 */
+	public void close(Statement st) {
+		close(st);
 	}
-	
+
 	/*
-	 * Cierra los objetos PreparedStatement, ResultSet y Connection simultaneamente.
+	 * Cierra los objetos PreparedStatement, ResultSet y Connection
+	 * simultaneamente.
 	 * 
-	 * @param ps   Objeto PreparedStatement. 
-	 * @param rs   Objeto ResultSet.
+	 * @param ps Objeto PreparedStatement.
+	 * 
+	 * @param rs Objeto ResultSet.
+	 * 
 	 * @param conn Objeto Connection.
-	 */			
-	public void closeAll(PreparedStatement ps, ResultSet rs, Connection conn){
-		closePs(ps);
-		closeRs(rs);
-		closeConn(conn);
+	 */
+	public void close(PreparedStatement ps, ResultSet rs, Connection conn) {
+		close(ps);
+		close(rs);
+		close(conn);
 	}
-	
+
 	/*
 	 * Cierra los objetos PreparedStatement y ResultSet simultaneamente.
 	 * 
-	 * @param ps Objeto PreparedStatement. 
+	 * @param ps Objeto PreparedStatement.
+	 * 
 	 * @param rs Objeto ResultSet.
-	 */	
-	public void closeAll(PreparedStatement ps, ResultSet rs){
-		closePs(ps);
-		closeRs(rs);
+	 */
+	public void close(PreparedStatement ps, ResultSet rs) {
+		close(ps);
+		close(rs);
 	}
-	
-	
+
 	/*
-	 * Retorna el arreglo con los indices de las consultas que no retornaron resultados con el numero de filas actualizadas,
-	 * lo que significa que en el proceso update no se encontro el id de la ruta.
-	 */	
-	public ArrayList<Integer> getIndexBatchUpdateException(BatchUpdateException bue){
+	 * Retorna el arreglo con los indices de las consultas que no retornaron
+	 * resultados con el numero de filas actualizadas, lo que significa que en
+	 * el proceso update no se encontro el id de la ruta.
+	 */
+	public ArrayList<Integer> getIndexBatchUpdateException(
+			BatchUpdateException bue) {
 		ArrayList<Integer> listaIndices = new ArrayList<Integer>();
 		int[] counts = bue.getUpdateCounts();
 		for (int i = 0; i < counts.length; i++) {
-			if(counts[i] == Statement.SUCCESS_NO_INFO){
+			if (counts[i] == Statement.SUCCESS_NO_INFO) {
 				listaIndices.add(new Integer(i));
 			}
 		}
-	    return listaIndices;
+		return listaIndices;
 	}
 }

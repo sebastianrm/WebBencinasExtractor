52accde6e32b97cd37e9f3125179643962e09d33
sebastianrm <retamal.sebastian@gmail.com>
2012-08-10 17:54:49 -0400

    desarrollo

--- WebExtractor/sql/creacion de schema.sql
+++ WebExtractor/sql/creacion de schema.sql
@@ -5,7 +5,7 @@ CREATE DATABASE `bencineras`;
 
 
 
-CREATE TABLE `regiones` (
+CREATE TABLE `bencineras`.`regiones` (
   `nombre` varchar(100) NOT NULL,
   PRIMARY KEY (`nombre`),
   UNIQUE KEY `nombre_UNIQUE` (`nombre`)
@@ -15,13 +15,13 @@ CREATE TABLE `regiones` (
 
 
 
-CREATE TABLE `servicentros` (
+CREATE TABLE `bencineras`.`servicentros` (
   `empresa` varchar(100) NOT NULL,
   `direccion` varchar(150) NOT NULL,
   `latitud` float NOT NULL DEFAULT '0',
   `longitud` float NOT NULL DEFAULT '0',
   `fkregion` varchar(100) NOT NULL,
-  PRIMARY KEY (`empresa`,`direccion`,`fkregion`),
+  PRIMARY KEY (`empresa`,`direccion`,`fkregion`,`latitud`,`longitud`),
   KEY `fk_reg` (`fkregion`),
   CONSTRAINT `fk_reg` FOREIGN KEY (`fkregion`) REFERENCES `regiones` (`nombre`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB;
@@ -29,7 +29,7 @@ CREATE TABLE `servicentros` (
 
 
 
-CREATE TABLE `bencinas` (
+CREATE TABLE `bencineras`.`bencinas` (
   `nombre` varchar(100) NOT NULL,
   PRIMARY KEY (`nombre`),
   UNIQUE KEY `nombre_UNIQUE` (`nombre`)
@@ -38,25 +38,26 @@ CREATE TABLE `bencinas` (
 
 
 
-CREATE TABLE `precios` (
+CREATE TABLE `bencineras`.`precios` (
   `precio` float NOT NULL DEFAULT '0',
   `fkbencina` varchar(100) NOT NULL,
   `fkempresa` varchar(100) NOT NULL,
   `fkdireccion` varchar(150) NOT NULL,
   `fkregion` varchar(100) NOT NULL,
   `fecha_actualizacion` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`precio`,`fkbencina`,`fkempresa`,`fkdireccion`,`fkregion`,`fecha_actualizacion`),
+  `fklatitud` float NOT NULL DEFAULT '0',
+  `fklongitud` float NOT NULL DEFAULT '0',
+  PRIMARY KEY (`precio`,`fkbencina`,`fkempresa`,`fkdireccion`,`fkregion`,`fecha_actualizacion`,`fklatitud`,`fklongitud`),
   KEY `fkbencina` (`fkbencina`),
-  KEY `fkservicentro` (`fkempresa`,`fkdireccion`,`fkregion`),
+  KEY `fkservicentro` (`fkempresa`,`fkdireccion`,`fkregion`,`fklatitud`,`fklongitud`),
   CONSTRAINT `fkbencina` FOREIGN KEY (`fkbencina`) REFERENCES `bencinas` (`nombre`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fkservicentro` FOREIGN KEY (`fkempresa`, `fkdireccion`, `fkregion`) REFERENCES `servicentros` (`empresa`, `direccion`, `fkregion`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `fkservicentro` FOREIGN KEY (`fkempresa`, `fkdireccion`, `fkregion`,`fklatitud`,`fklongitud`) REFERENCES `servicentros` (`empresa`, `direccion`, `fkregion`,`latitud`,`longitud`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB;
 
 
 
 
-
-CREATE TABLE `direcciones_google` (
+CREATE TABLE `bencineras`.`direcciones_google` (
   `fkempresa` varchar(60) NOT NULL,
   `fkdireccion` varchar(100) NOT NULL,
   `fkregion` varchar(60) NOT NULL,
@@ -65,5 +66,3 @@ CREATE TABLE `direcciones_google` (
   KEY `fk_servicentro` (`fkempresa`,`fkdireccion`,`fkregion`),
   CONSTRAINT `fk_servicentro` FOREIGN KEY (`fkempresa`, `fkdireccion`, `fkregion`) REFERENCES `servicentros` (`empresa`, `direccion`, `fkregion`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB ;
-
-
